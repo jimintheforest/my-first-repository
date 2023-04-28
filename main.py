6942ae46f194d6e60bc51d53cd0f1ca3cc4ff01b
jimintheforest <131692473+jimintheforest@users.noreply.github.com>
2023-04-28 14:56:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+class Transaction:
+    def __init__(self, currency, amount, exchange_rate):
+        self.currency = currency
+        self.amount = amount
+        self.exchange_rate = exchange_rate
+
+
+def user_input():
+    currency = input("Enter the currency: ")
+    amount = input("Enter the amount: ")
+    exchange_rate = input("Enter the exchange rate: ")
+    return Transaction(currency, amount, exchange_rate)
+
+
+new_transaction = user_input()
+
+print(new_transaction.currency, new_transaction.amount, new_transaction.exchange_rate)
+
+transaction_list = []
+
+
+def add_transaction():
+    transaction = new_transaction
+    return transaction_list.append(transaction)
+
+
+add_transaction()
+
+print(transaction_list)
